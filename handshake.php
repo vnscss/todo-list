9de8df96d186165bad5307709b46bf884d72d45e
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-06-07 15:45:48 -0300

    Feat: salvando os hashs em uma tabela

--- handshake.php
+++ handshake.php
@@ -10,6 +10,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $salt = "hadcodedsalt";
 
     $hash = $clientUnix . $salt . $randon . rand();
-    echo hash('sha256', $hash);
+    $hash = hash('sha256', $hash);
+
+    try {
+        $myPDO = new PDO('sqlite:./data.db');
+        $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $myPDO->prepare("INSERT INTO hashes (hash) VALUES (:hash)");
+        $stmt->execute([
+            ':hash' => $hash
+        ]);
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    } catch (PDOException $e) {
+        echo "Erro" . $e->getMessage();
+    }
+
+    echo $hash;
 }
 ?>
