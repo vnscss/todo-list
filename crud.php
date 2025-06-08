5e8915b5d76a6b09e9bc38a9718f77e0f0364649
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-06-08 18:42:14 -0300

    Feat: validação dos hashs antes de executar o crud

--- crud.php
+++ crud.php
@@ -13,105 +13,125 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $clientHash = $_POST['b'] ?? '';
     $task = $_POST['c'];
 
-    $data = json_decode($task, true); // true para array associativo
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        echo "JSON inválido.";
-        exit;
-    }
 
-    $id = $data['id'];
-    $name = $data['name'];
-    $descricao = $data['descricao'] ?? null;
-    $dataUnix = $data['data'];
+        try {
+        $myPDO = new PDO('sqlite:./data.db');
+        $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        $stmt = $myPDO->prepare("SELECT hash FROM hashes WHERE hash = :hash");
+        $stmt->bindParam(':hash', $clientHash, PDO::PARAM_STR);
+        $stmt->execute();
 
-    switch ($type) {
-        case 1:
-            try {
-                $myPDO = new PDO('sqlite:./data.db');
-                $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $hash = $result[0]['hash'];
+        } catch (PDOException $e) {
+            echo "Erro: " . $e->getMessage();
+        }
 
-                $stmt = $myPDO->prepare("INSERT INTO tarefas (name, descricao, done, data, user) VALUES (:name, :descricao, 0, :data, :user)");
-                $stmt->execute([
-                    ':name' => $name,
-                    ':descricao' => $descricao,
-                    ':data' => $dataUnix,
-                    ':user' => $clientHash
-                ]);
+        if($hash == $clientHash){
+                $data = json_decode($task, true); // true para array associativo
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    echo "JSON inválido.";
+                    exit;
+                }
 
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                echo json_encode($result);
-
-            } catch (PDOException $e) {
-                echo "Erro: " . $e->getMessage();
-            }
-            break;
-
-        case 2:
-            try {
-                $myPDO = new PDO('sqlite:./data.db');
-                $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                $stmt = $myPDO->prepare(" UPDATE tarefas SET name = :name, descricao = :descricao, data = :data WHERE id = :id AND user = :user");
-                $stmt->execute([
-                    ':name' => $name,
-                    ':descricao' => $descricao,
-                    ':data' => $dataUnix,
-                    ':id' => $id,
-                    ':user' => $clientHash
-                ]);
-
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                echo json_encode($result);
-
-            } catch (PDOException $e) {
-                echo "Erro: " . $e->getMessage();
-            }
-            break;
-
-        case 3:
-            try {
-                $myPDO = new PDO('sqlite:./data.db');
-                $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $id = $data['id'];
+                $name = $data['name'];
+                $descricao = $data['descricao'] ?? null;
+                $dataUnix = $data['data'];
 
 
-                $stmt = $myPDO->prepare("DELETE FROM tarefas WHERE id = :id AND user = :user");
-                $stmt->execute([
-                    ':id' => $id,
-                    ':user' => $clientHash
-                ]);
+                switch ($type) {
+                    case 1:
+                        try {
+                            $myPDO = new PDO('sqlite:./data.db');
+                            $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                echo json_encode($result);
+                            $stmt = $myPDO->prepare("INSERT INTO tarefas (name, descricao, done, data, user) VALUES (:name, :descricao, 0, :data, :user)");
+                            $stmt->execute([
+                                ':name' => $name,
+                                ':descricao' => $descricao,
+                                ':data' => $dataUnix,
+                                ':user' => $clientHash
+                            ]);
 
-            } catch (PDOException $e) {
-                echo "Erro: " . $e->getMessage();
-            }
-            break;
+                            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                            echo json_encode($result);
 
-        case 4:
-            try {
-                $myPDO = new PDO('sqlite:./data.db');
-                $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        } catch (PDOException $e) {
+                            echo "Erro: " . $e->getMessage();
+                        }
+                        break;
+
+                    case 2:
+                        try {
+                            $myPDO = new PDO('sqlite:./data.db');
+                            $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                            $stmt = $myPDO->prepare(" UPDATE tarefas SET name = :name, descricao = :descricao, data = :data WHERE id = :id AND user = :user");
+                            $stmt->execute([
+                                ':name' => $name,
+                                ':descricao' => $descricao,
+                                ':data' => $dataUnix,
+                                ':id' => $id,
+                                ':user' => $clientHash
+                            ]);
+
+                            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                            echo json_encode($result);
+
+                        } catch (PDOException $e) {
+                            echo "Erro: " . $e->getMessage();
+                        }
+                        break;
+
+                    case 3:
+                        try {
+                            $myPDO = new PDO('sqlite:./data.db');
+                            $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-                $stmt = $myPDO->prepare("UPDATE tarefas SET done = 1 WHERE id = :id AND user = :user");
-                $stmt->execute([
-                    ':id' => $id,
-                    ':user' => $clientHash
-                ]);
+                            $stmt = $myPDO->prepare("DELETE FROM tarefas WHERE id = :id AND user = :user");
+                            $stmt->execute([
+                                ':id' => $id,
+                                ':user' => $clientHash
+                            ]);
 
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                echo json_encode($result);
+                            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                            echo json_encode($result);
 
-            } catch (PDOException $e) {
-                echo "Erro: " . $e->getMessage();
-            }
-            break;
+                        } catch (PDOException $e) {
+                            echo "Erro: " . $e->getMessage();
+                        }
+                        break;
 
-        default:
-            echo "error";
-            break;
-    }
+                    case 4:
+                        try {
+                            $myPDO = new PDO('sqlite:./data.db');
+                            $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+                            $stmt = $myPDO->prepare("UPDATE tarefas SET done = 1 WHERE id = :id AND user = :user");
+                            $stmt->execute([
+                                ':id' => $id,
+                                ':user' => $clientHash
+                            ]);
+
+                            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                            echo json_encode($result);
+
+                        } catch (PDOException $e) {
+                            echo "Erro: " . $e->getMessage();
+                        }
+                        break;
+
+                    default:
+                        echo "error";
+                        break;
+                }
+        }
+        else{
+            return;
+        }
 }
 ?>
